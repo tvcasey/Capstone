2631de5024dcf0f0dd3ff87dc7a4e0310637ae91
Timothy Casey <tvcasey@yahoo.com>
2021-01-13 13:05:09 -0500

    Finished the SignIn.  Added to the README.  There are lots of little things that could be done, but I did get a lot done.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Capstone
+
+Timothy Casey
+devCodeCamp, Milwaukee, WI
+Completion of Final Project: Capstone, January 13, 2021
+
+
+I learned a lot about the complete MERN Stack as it was my first real project on my own.  I used a dozen or more tutorials along with class notes and class projects to come up with my finished product.  The instructors were key at times when I exhausted everything I knew or didn't know to progress to the next step.  This is definitely not a production quality result as there needs to be some better use of the financial information, like a company search.  Also, I need to CSS/bootstrap better.  I may need to provide more than a blog maybe.  A newsletter may be a good addition.  The Alpha Vantage API is a powerful tool to access relevant and timely accounting information.  I am pleased to find about it and to maybe come up with a professional site in the future with the MERN stack and Alpha Vantage API.
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,37 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios';
 
 
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
 
-const SignIn = ( props ) => {
+        this.state = {  
+            email: "",
+            password: ""
 
+        }
 
+        this.changeEmail = this.changeEmail.bind(this);
+        this.changePassword = this.changePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+    changeEmail(event) {
+        this.setState({
+            email: event.target.value
+        });
+
+    }
+    changePassword(event) {
+        this.setState({
+            password: event.target.value
+        });
+
+    }
+    onSubmit(event) {
+        event.preventDefault();
+
+        const users = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios.post('http://localhost:8080/api/users/signin', users)
+            .then(response => console.log(response.data))
+    
+        this.setState({
+            email: '',
+            password: ''
+        })
+        }
+
+    render() {
     return (
         <div className='container'>
-        <Form className='row-6-md'>
+        <Form onSubmit={this.onSubmit} className='row-6-md'>
             <Label><h4>LOG IN</h4></Label>
             <FormGroup>
                 <Label for='exampleEmail'><h4>Email</h4></Label>
-                <Input type='email' name='email' id='exampleEmail' placeholder='Please enter your email.' />
+                <Input type='email' 
+                       name='email' 
+                       placeholder='Please enter your email.' 
+                       value={this.state.email} 
+                       onChange={this.changeEmail} 
+                       />
             </FormGroup>
             <FormGroup>
                 <Label for='examplePassword'><h4>Password</h4></Label>
-                <Input type='password' name='password' id='examplePassword' placeholder='Please enter your password.' />    
+                <Input type='password' 
+                       name='password' 
+                       placeholder='Please enter your password.' 
+                       value={this.state.password} 
+                       onChange={this.changePassword} 
+                       />    
             </FormGroup>
-            <Button>Submit</Button>
+            <Button type='Submit'>Log In</Button>
         </Form>
         </div>
 
     )
-
-
-
-
-
-
+    }
 }
 
 
 
 
-export default SignIn;
+
+
+
+
+
